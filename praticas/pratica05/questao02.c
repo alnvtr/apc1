8862f297626ead71d611df04a0dfb992cf7a244a
alnvtr <142408525+alnvtr@users.noreply.github.com>
2023-10-02 13:54:29 +0000

    .

---
+++ praticas/pratica05/questao02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+float valor_bruto = 0 
+  printf("insira um valor do produto:");
+  scanf("%f", &valor_produto);
+
+  if (valor_bruto <= 100){
+    int desconto = valor_bruto / 0.01f
+    int valor_com_desconto = valor_bruto - desconto
+      printf("o valor do produto com desconto fica: %i\n",valor_com_desconto)
+    
+  }
+ 
+
+
+  return 0;
+}
